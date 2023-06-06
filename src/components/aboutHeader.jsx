6c92a2c2ee666ba9263b7fb180a4d416fd0137c3
Rahma <rahmamahdi.02@gmail.com>
2023-06-06 06:06:49 -0700

    update text

---
+++ src/components/aboutHeader.jsx
@@ -0,0 +1,13 @@
+import { Row } from "react-bootstrap"
+import '/src/components/HeaderSection.css'
+
+function HeaderSection() {
+	return (
+		<div class="row row_center blue-background">
+			<h1 class="h1-white-text">Contributors</h1>
+            <h3 class="h3-white-text"> Meet the team behind Techtonica's Question Builder </h3>
+		</div>
+	);
+}
+
+export default HeaderSection;
