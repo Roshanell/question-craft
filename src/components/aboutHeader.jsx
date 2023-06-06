921c5c566fa4922fe4c7f24a0675da0aec3d14c7
Rahma <rahmamahdi.02@gmail.com>
2023-06-06 08:08:27 -0700

    remove header

--- src/components/aboutHeader.jsx
+++ src/components/aboutHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 const styles = {
     container: {
       display: 'flex',
@@ -15,12 +16,13 @@ const styles = {
     },
     
   };
+  
  function HeaderSection() {
      	return (
             <div style={styles.container}>
             <div style={styles.hero}>
               <h1>Contributors</h1>
-              <h3 class="h3-white-text"> Meet the team behind Techtonica's Question Builder </h3>
+              <h3> Meet the team behind Techtonica's Question Builder </h3>
             </div>
  		</div>
  	);
