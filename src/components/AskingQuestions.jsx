843cf7914f5b0cba14a24e1038a6a44ecda63e24
TanyaPina <tanyapina15@gmail.com>
2023-06-05 19:54:01 -0700

    initial component set-up

---
+++ src/components/AskingQuestions.jsx
@@ -0,0 +1,11 @@
+import '/src/components/AskingQuestions.css'
+
+function AskingQuestions() {
+	return (
+		<div>
+			<h1>Response will go here</h1>
+		</div>
+	);
+}
+
+export default AskingQuestions;
