8b7dc03d60b59fe2df12c4b585419ed3c9e61f87
TanyaPina <tanyapina15@gmail.com>
2023-06-05 19:19:22 -0700

    centered header text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,10 @@
-import { Row } from "react-bootstrap";
+import { Row } from "react-bootstrap"
+import '/src/components/HeroSection.css'
 
 function HeroSection() {
 	return (
-		<div class="row">
-			<h1>Welcome to Techtonica's Question Builder!</h1>
+		<div class="row row_center blue-background">
+			<h1 class="large-white-text">Welcome to Techtonica's Question Builder!</h1>
 		</div>
 	);
 }
