02ad5511ed00552303cc1739139ccbcfe47a5686
Rahma <rahmamahdi.02@gmail.com>
2023-06-06 07:54:32 -0700

    update text

--- src/components/bioCards.jsx
+++ src/components/bioCards.jsx
@@ -14,17 +14,13 @@ const Bio = () => {
             <Card className="mb-3">
               <Row noGutters>
                 <Col md={4}>
-                  <Image src="path_to_image1.jpg" roundedCircle className="p-3" />
+                  <Image src="https://miro.medium.com/v2/resize:fit:1000/format:webp/1*JNoIODyECscyVkkSwuVXzw.png" roundedCircle className="p-3" />
                 </Col>
-                <Col md={8}>
+                <Col md={10}>
                   <Card.Body>
-                    <Card.Title>Card 1</Card.Title>
+                    <Card.Title>Meet Camille (she/her, they/them)</Card.Title>
                     <Card.Text>
-                      Line 1<br />
-                      Line 2<br />
-                      Line 3<br />
-                      Line 4<br />
-                      Line 5
+                    After having led adult learners as a teacher with Teach For America, Camille is a proud participant of the Techtonica program, and excited to have the opportunity to learn again after having been out of school for many years. She is based in Philadelphia, but grew up in South Florida, where she fostered a love of singing and building community. About two years into the pandemic, Camille began craving an opportunity to learn new skills, and grow in a new field. They began learning how to code via freeCodeCamp and Skillcrush, before applying to Techtonica and learning how to build full stack applications. She is hopeful about the future and confident in her abilities to go far in this new field.
                     </Card.Text>
                   </Card.Body>
                 </Col>
@@ -34,17 +30,13 @@ const Bio = () => {
             <Card className="mb-3">
               <Row noGutters>
                 <Col md={4}>
-                  <Image src="path_to_image2.jpg" roundedCircle className="p-3" />
+                  <Image src="https://miro.medium.com/v2/resize:fit:1000/format:webp/1*K4UH3KOsovJE7fzqdA23Fg.png" roundedCircle className="p-3" />
                 </Col>
-                <Col md={8}>
+                <Col md={10}>
                   <Card.Body>
-                    <Card.Title>Card 2</Card.Title>
+                    <Card.Title>Meet Roshanell (she/her)</Card.Title>
                     <Card.Text>
-                      Line 1<br />
-                      Line 2<br />
-                      Line 3<br />
-                      Line 4<br />
-                      Line 5
+                    Roshanell is an inspiring educator-turned-software engineer, bringing a unique blend of skills to the tech industry. Her fascination with leveraging software tools to enrich classroom learning and streamline lesson-planning led her to Techtonica, an organization dedicated to nurturing diversity in tech. During her time at Techtonica, Roshanell developed an innovative AI-powered application to revolutionize educational practices, further blending her passion for technology and education. With her newly-minted coding skills and deep educational insights, Roshanell stands poised to make significant contributions to any tech team, bringing a fresh and impactful perspective to software development.
                     </Card.Text>
                   </Card.Body>
                 </Col>
@@ -54,17 +46,13 @@ const Bio = () => {
             <Card className="mb-3">
               <Row noGutters>
                 <Col md={4}>
-                  <Image src="path_to_image3.jpg" roundedCircle className="p-3" />
+                  <Image src="https://miro.medium.com/v2/resize:fit:1000/format:webp/1*ZzUGWgV_iSVhWHHtQVPHSw.png" roundedCircle className="p-3" />
                 </Col>
-                <Col md={8}>
+                <Col md={10}>
                   <Card.Body>
-                    <Card.Title>Card 3</Card.Title>
+                    <Card.Title>Meet Tanya (they/them) </Card.Title>
                     <Card.Text>
-                      Line 1<br />
-                      Line 2<br />
-                      Line 3<br />
-                      Line 4<br />
-                      Line 5
+                    A life-long learner, Tanya is passionate about the intersection between tech and humans. Prior to joining Techtonica, they served as a library assistant where they used their skills acquired from their anthropology degree to coordinate various community events and programs. Their curiosity in tech grew as they led digital literacy classes and began to immerse themselves in the HTML, CSS, and the world of tech. They are excited to integrate their previous skills with those learned at Techtonica in order to create inclusive tech spaces by and for underrepresented communities.
                     </Card.Text>
                   </Card.Body>
                 </Col>
@@ -74,17 +62,13 @@ const Bio = () => {
             <Card className="mb-3">
               <Row noGutters>
                 <Col md={4}>
-                  <Image src="path_to_image4.jpg" roundedCircle className="p-3" />
+                  <Image src="https://miro.medium.com/v2/resize:fit:1000/format:webp/1*Oxhs2VdlOI1z1javQI1s4w.jpeg" roundedCircle className="p-3" />
                 </Col>
                 <Col md={8}>
                   <Card.Body>
-                    <Card.Title>Card 4</Card.Title>
+                    <Card.Title>Meet Rahma (she/her) </Card.Title>
                     <Card.Text>
-                      Line 1<br />
-                      Line 2<br />
-                      Line 3<br />
-                      Line 4<br />
-                      Line 5
+                    Rahma is a San Francisco Bay Area native and undergraduate at UC Berkeley where she serves as Regents & Chancellors Scholar. Her initial introduction to coding was as a Research Apprentice, where she honed her analytical prowess in utilizing Python libraries for mixed-method data analysis. After an internship for an NGO in East Africa, she was inspired to build web application solutions tackling critical issues such as telemedicine care in rural Kenya. As a software engineer, Rahma is committed to building transformative products that inspire innovation and reach diverse communities worldwide.
                     </Card.Text>
                   </Card.Body>
                 </Col>
