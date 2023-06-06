b74b80451f16945d07957cdef05bfe92972b521e
TanyaPina <tanyapina15@gmail.com>
2023-06-05 19:37:10 -0700

    added and formatted tagline

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,8 @@ import '/src/components/HeroSection.css'
 function HeroSection() {
 	return (
 		<div class="row row_center blue-background">
-			<h1 class="large-white-text">Welcome to Techtonica's Question Builder!</h1>
+			<h1 class="h1-white-text">Welcome to Techtonica's Question Builder</h1>
+            <h3 class="h3-white-text"> Taking the guesswork out of building a great question!</h3>
 		</div>
 	);
 }
