83cda3075f3145020f064045276edb8f3c6aa184
TanyaPina <tanyapina15@gmail.com>
2023-06-05 20:09:18 -0700

    added questions paragraph

--- src/components/AskingQuestions.jsx
+++ src/components/AskingQuestions.jsx
@@ -3,7 +3,11 @@ import '/src/components/AskingQuestions.css'
 function AskingQuestions() {
 	return (
 		<div class="row row_center">
-			<h1>Response will go here</h1>
+            <div class="column">
+            <p class="justify">Why ask questions? The ability to ask questions that lead to useful answers is a must-have skill for 
+            software engineers because they are constantly solving problems. Asking the right questions helps engineers arrive at 
+            solutions more quickly and make faster progress.</p>
+            </div>
 		</div>
 	);
 }
