57c3abcd51b2567932e61744387af8af1177a48c
Rahma <rahmamahdi.02@gmail.com>
2023-06-07 08:11:50 -0700

    final touches

--- client/src/components/AboutHeader.jsx
+++ client/src/components/AboutHeader.jsx
@@ -4,7 +4,7 @@ import '/src/components/HeaderSection.css'
 function AboutHeader() {
 	return (
 		<div className="row row_center blue-background">
-			<h1 className="h1-white-text">About Us</h1>
+			<h1 className="h1-white-text">About</h1>
             <h3 className="h3-white-text"> Taking the guesswork out of building a great question!</h3>
 		</div>
 	);
