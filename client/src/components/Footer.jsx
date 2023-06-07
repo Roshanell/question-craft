1f503d6d655743bccd0b819d71f2642aedac5329
Rahma <rahmamahdi.02@gmail.com>
2023-06-06 19:46:59 -0700

    footer updated

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container d-flex justify-content-center align-items-center">
+        <span className="text-muted"> Need More Help? Contact Us <Link to="/about">Here</Link> </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
